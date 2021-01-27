125b0e5d95d631ddaa75388c2c5b16fec012ecd1
Swdenis <swdenis@mail.ru>
2021-01-27 21:41:36 +0100

    Added a video player

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,59 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-    state={videos: [],}
+    state={
+        videos: [], 
+        selectedVideo: null,
+    }
+
+    componentDidMount() {
+        this.onTermSubmit('most popular videos');
+    }
 
     onTermSubmit = async term => 
     {
-        const reponse = await youtube.get(
+        const response = await youtube.get(
             '/search',{
                 params: {
                     q: term
                 }
             }
         )
-        this.setState({videos: reponse.data.items});
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     }
+
+    onVideoSelect = video => {
+        this.setState({selectedVideo: video});
+    }
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar
                 onFormSubmit={this.onTermSubmit}
                 />
-                <VideoList videos={this.state.videos} />
+                <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                    <VideoDetail 
+                video={this.state.selectedVideo}
+                />
+                    </div>
+                    <div className="five wide column">
+                    <VideoList 
+                videos={this.state.videos}
+                onVideoSelect = {this.onVideoSelect}
+                 />
+                    </div>
+                </div>
+                </div>
+                
             </div>
             );
     }
